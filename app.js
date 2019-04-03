83743d540def8f28b9d3859d1dd8eeac99ee2fea
bishalDores <lazarisalive@gmail.com>
2019-04-03 07:56:00 +0600

    displaying budget on the UI

--- app.js
+++ app.js
@@ -96,8 +96,12 @@ var UIController = (function () {
         inputValue: '.add__value',
         inputBtn: '.add__btn',
         incomeContainer: '.income__list',
-        expenseContainer: '.expenses__list'
-    }
+        expenseContainer: '.expenses__list',
+        budgetLabel: '.budget__value',
+        incomeLabel:'.budget__income--value',
+        expensesLabel:'.budget__expenses--value',
+        percentageLabel:'.budget__expenses--percentage'
+    };
 
     return {
         getInput: function () {
@@ -141,6 +145,16 @@ var UIController = (function () {
             fieldsArr[0].focus();
             console.log(fieldsArr);
         },
+        displayBudget:function(obj){
+            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+            document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+            if(obj.percentage > 0){
+                document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+            }else{
+                document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+            }
+        },
         getDOMstrings: function () {
             return DOMstrings;
         }
@@ -172,6 +186,7 @@ var controller = (function (budgetCtrl, UICtrl) {
         //2. Return the budget
         var budget = budgetCtrl.getBudget();
         //3. Display the budget on the UI
+        UICtrl.displayBudget(budget);
         console.log(budget);
     }
 
@@ -201,6 +216,12 @@ var controller = (function (budgetCtrl, UICtrl) {
     return {
         init: function () {
             console.log('Application has started');
+            UICtrl.displayBudget({
+                budget:0,
+                totalInc:0,
+                totalExp:0,
+                percentage:-1
+            });
             setupEventListerners();
         }
     }
